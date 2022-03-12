1e661b67bd45303b40e760f8c56a52982c715c2b
abhishekkatara <abhikatara.sanskar1@gmail.com>
2022-03-12 20:32:46 +0530

    function: Search by track done

--- src/shared/Search.jsx
+++ src/shared/Search.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchService from "../services/Search.service"
 
 
-const Search = ({searchIn}) => (
-    <div id="parent" >Search by {searchIn}
-        <div>
-           <input type="text" id="search" placeholder="Search..." />
+
+const Search = ({searchIn}) => {
+    const [ searchTxt, setSearchTxt ] = useState('');
+    return (
+        <div id="parent" >Search by {searchIn}
+            <div>
+            <input type="text" id="search" placeholder="Search..." onChange={event => { setSearchTxt(event.target.value); }}
+                onKeyPress={event => {
+                if (event.key === 'Enter') {
+                    SearchService('track', searchTxt)
+                }
+            }}
+            />
+            </div>
         </div>
-    </div>
-    
-)
+    )
+}
 
 export default Search;
